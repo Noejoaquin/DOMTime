e24350ce72b5a51673a8b5910df46c1daa3fd6aa
Noe <noe.joaquin.james@gmail.com>
2017-11-16 20:39:23 -0500

    styling

--- demo/todo.js
+++ demo/todo.js
@@ -31,43 +31,43 @@ $l(() => {
   })
   
   
-  const button = $l('<button>');
-  let buttonCount = 1
-  button.addClass('new-button')
-  button.append('lets make some more buttons! CLICK ME')
-  $l('.create-button-div').append(button)
-  $l('.new-button').on('click', () => {
-    // debugger
-    const addedButton = $l('<button>');
-    addedButton.addClass(`added-button-${buttonCount}`);
-    addedButton.addClass('created-button')
-    addedButton.append('click if you dare')
-    let newLi = $l('<li>')
-    newLi.append(addedButton)
-    $l('.created-buttons').append(newLi)
-    $l(`.added-button-${buttonCount}`).on('click', () => {
-      $l('body').toggleClass('change-color')
-    })
-    buttonCount++;
-  })
+  // const button = $l('<button>');
+  // let buttonCount = 1
+  // button.addClass('new-button')
+  // button.append('lets make some more buttons! CLICK ME')
+  // $l('.create-button-div').append(button)
+  // $l('.new-button').on('click', () => {
+  //   // debugger
+  //   const addedButton = $l('<button>');
+  //   addedButton.addClass(`added-button-${buttonCount}`);
+  //   addedButton.addClass('created-button')
+  //   addedButton.append('click if you dare')
+  //   let newLi = $l('<li>')
+  //   newLi.append(addedButton)
+  //   $l('.created-buttons').append(newLi)
+  //   $l(`.added-button-${buttonCount}`).on('click', () => {
+  //     $l('body').toggleClass('change-color')
+  //   })
+  //   buttonCount++;
+  // })
   
   
-  const h1Input = $l('<input>')
-  h1Input.addClass('h1-generator-input')
-  $l('.update-todo-header-container').append(h1Input)
-  const changeTitleButton = $l('<button>')
-  changeTitleButton.addClass('todo-h1-generator-button')
-  changeTitleButton.append('Update title')
-  $l('.update-todo-header-container').append(changeTitleButton)
-  $l('.todo-h1-generator-button').on('click', () => {
-    let newH1 = $l('<h1>')
-    // debugger
-    let h1Content = $l('.h1-generator-input').htmlArray[0].value
-    newH1.append(h1Content)
-    $l('.todo-header').remove()
-    newH1.addClass('todo-header')
-    $l('.todo-header-container').append(newH1)
-  })
+  // const h1Input = $l('<input>')
+  // h1Input.addClass('h1-generator-input')
+  // $l('.update-todo-header-container').append(h1Input)
+  // const changeTitleButton = $l('<button>')
+  // changeTitleButton.addClass('todo-h1-generator-button')
+  // changeTitleButton.append('Update title')
+  // $l('.update-todo-header-container').append(changeTitleButton)
+  // $l('.todo-h1-generator-button').on('click', () => {
+  //   let newH1 = $l('<h1>')
+  //   // debugger
+  //   let h1Content = $l('.h1-generator-input').htmlArray[0].value
+  //   newH1.append(h1Content)
+  //   $l('.todo-header').remove()
+  //   newH1.addClass('todo-header')
+  //   $l('.todo-header-container').append(newH1)
+  // })
   
   // $l('weather-button').on('click', () => {
   //   $l.ajax({
